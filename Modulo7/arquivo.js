877ee38eaf12792914c0d86c08915bf2c18b9988
Erick <erickplacides@gmail.com>
2022-06-14 20:19:45 -0300

    Mod 07

---
+++ Modulo7/arquivo.js
@@ -0,0 +1,25 @@
+ /* aula 3
+alert(' Seja Bem Vindo!!!');
+
+document.write('Nossa primeira aula JS');
+document.querySelector('#h2').textContent = 'Terceiro teste'; */
+
+/* exemplo de como exibir caixa dialogo
+
+const nome = prompt('Qual é seu nome?');
+
+alert('Que bom ter você aqui ${nome}! \n Vamos começar?'); */
+
+/* Aula 4 
+
+function bemVindo(){
+    const nome = document.querySelector('#nomeUsuario').value;
+    alert('Olá '   + (nome) +  ' Seja bem-vindo!');
+    document.getElementById('formularioLogin').style.display = 'none';
+}*/
+
+function mudaCorFundo(){
+  const menu = document.querySelector (`#coresFundo`) 
+  const cor = menu.options[menu.seletedIndex].value;
+  document.body.style.background = cor;
+}
