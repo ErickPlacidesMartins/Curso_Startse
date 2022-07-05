524272317e982d38e9074a0d16ccc3db0f6288af
Erick <erickplacides@gmail.com>
2022-07-04 21:00:33 -0300

    Rev04/07/22

---
+++ Modulo9/index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+/*class Filme {
+    constructor(titulo, ano) {
+        this.titulo = titulo;
+        this.ano = ano;
+    }
+}
+
+let meuFilme = new Filme('aprendendo JS', 2022);
+console.log(meuFilme); 
+
+class Produto {
+    constructor() {
+        this.tipo = 'Produto X'
+    }
+}
+
+const prod = new Produto ();
+console.log(prod.tipo);
+
+//Formato sugerido para criação de clases
+
+class Financiamento {
+    valorFinanciamento;
+    taxaJuros;
+    constructor(valorFinanciamento, taxaJuros){
+        this.valorFinanciamento = valorFinanciamento;
+        this.taxaJuros = taxaJuros;
+    }
+}
+
+const fin = new Financiamento(1000, 2);
+console.log(fin) */
+
