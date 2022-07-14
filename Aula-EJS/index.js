0ccee4a4a2a768d6b6da1858036fd31adcf2356e
Erick <erickplacides@gmail.com>
2022-07-13 21:09:01 -0300

    final modulo 10

---
+++ Aula-EJS/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
+app.get('/', (request, response) => {
+    response.render('pages/home', {});
+});
